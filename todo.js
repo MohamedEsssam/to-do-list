61a865914ad15f076054777a7fc48ecf13323688
Mohamed Essam <me547010@gmail.com>
2018-08-23 09:05:06 +0200

    first commit

--- todo.js
+++ todo.js
@@ -1,7 +1,3 @@
-  var num=0;
-  var pos=0;
-  var x = false;
-  var btn=document.getElementById("add");
   /*document.getElementById("add").addEventListener("click",function(){
     var value=document.getElementById("value").value;
     if(value) addItem(value);
@@ -16,7 +12,8 @@
     
  });
  */
-//document.getElementById().style.a
+
+
 /*
 function completeItem(){
 
@@ -28,6 +25,11 @@ nums++;
 console.log(nums);
 }
 */
+var num=0;
+var pos=0;
+var x = false;
+var btn=document.getElementById("add");
+
 $(document).ready(function(){
     $("#add").click(function(){
         var value=document.getElementById("value").value;
@@ -38,6 +40,7 @@ $(document).ready(function(){
         $("#add").animate({right: "12"});
     });
 });
+
 document.getElementById().style.animation="";
 function removeItem() {
     var result = confirm("want to delele ?")
@@ -49,7 +52,7 @@ function removeItem() {
 }
 
 function addItem(value) {
- var nums=0;
+  var nums=0;
   var list=document.getElementById("todo");
 
   var item=document.createElement("li");
@@ -69,12 +72,13 @@ function addItem(value) {
     var val=this.parentNode;
     console.log(val);
     if(nums%2==0){
-    document.getElementById("complete").style.background="";
+    this.style.background="";
     val.style.transition="all 2s";
     val.style.background="rgba(255, 255, 255, 0.815)";
     }
     else{
-    document.getElementById("complete").style.background="rgba(6, 6, 250, 0.61)";
+    this.style.background="rgba(6, 6, 250, 0.61)";
+
     val.style.transition="all 2s";
     val.style.background="rgb(89, 129, 240)";
     }  
